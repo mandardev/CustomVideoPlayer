d31ea8deb3c07e0e08665da48a3689c1b30b69cb
Mandar Choudhary <mandar.developer7@gmail.com>
2019-11-21 22:05:46 +0530

    Initial Commit

---
+++ CustomVideoPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomVideoPlayer
+//
+//  Created by Mandar Choudhary on 21/11/19.
+//  Copyright © 2019 Mandar Choudhary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
