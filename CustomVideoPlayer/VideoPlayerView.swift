9eb43eccb837fa6d9bd88ea11ca13eebacb2f501
Mandar Choudhary <mandar.developer7@gmail.com>
2019-11-21 23:20:02 +0530

    First commit

---
+++ CustomVideoPlayer/VideoPlayerView.swift
@@ -0,0 +1,21 @@
+//
+//  VideoPlayerView.swift
+//  CustomVideoPlayer
+//
+//  Created by Mandar Choudhary on 21/11/19.
+//  Copyright © 2019 Mandar Choudhary. All rights reserved.
+//
+
+import UIKit
+
+class VideoPlayerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
